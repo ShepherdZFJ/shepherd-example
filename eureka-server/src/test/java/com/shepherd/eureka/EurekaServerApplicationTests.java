bc835b2cd2652417d6a560f33f84e89d79e5cdd1
fjzheng <1059959730@qq.com>
2022-06-08 15:24:15 +0800

    add：:sparkles:添加eureka server

---
+++ eureka-server/src/test/java/com/shepherd/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shepherd.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
