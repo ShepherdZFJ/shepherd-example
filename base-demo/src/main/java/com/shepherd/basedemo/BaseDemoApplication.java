7e5279f4f208cc4dfcebad971737ff17573360a1
fjzheng <1059959730@qq.com>
2024-05-13 16:15:14 +0800

    add: :rocket: 缓存

--- base-demo/src/main/java/com/shepherd/basedemo/BaseDemoApplication.java
+++ base-demo/src/main/java/com/shepherd/basedemo/BaseDemoApplication.java
@@ -3,11 +3,13 @@ package com.shepherd.basedemo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
 @MapperScan(basePackages = "com.shepherd.basedemo.dao")
+@EnableCaching
 public class BaseDemoApplication {
 
     public static void main(String[] args) {
